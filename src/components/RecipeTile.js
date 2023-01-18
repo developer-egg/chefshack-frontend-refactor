5e0c0416438130ebc91896b242c8a92925f057bb
egg <eggboissaye@gmail.com>
2023-01-17 20:01:03 -0500

    Made Recipes Page Connect to Backend

--- src/components/RecipeTile.js
+++ src/components/RecipeTile.js
@@ -1,7 +1,7 @@
 const RecipeTile = (props) => {
     return (
         <div className="recipe-tile">
-            <img src="https://www.healthylifestylesliving.com/wp-content/uploads/2015/12/placeholder-256x256.gif" alt="placeholder" />
+            <img src={props.image} alt="placeholder" width={256} height={256}/>
             <h2 className="mt-3">{props.title}</h2>
             <p>by {props.author.toUpperCase()}</p>
         </div>
--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,29 +1,60 @@
-import { Container, Col, Row } from "react-bootstrap";
+import { Container, Col, Row, Spinner } from "react-bootstrap";
 import RecipeTile from "../components/RecipeTile";
 
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Recipes = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function fillRecipeList() {
+    await axios
+      .get("http://localhost:8000/recipes/get_all_recipes")
+      .then((res) => {
+        setRecipes(res.data.recipes);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        // since all axios calls are done at this point, set isLoading to false
+        setIsLoading(false);
+      });
+  }
+
+  useEffect(() => {
+    fillRecipeList();
+  }, []);
+
+  if (!isLoading) {
     return (
-        <Container>
-            <div className="mt-5 mb-5">
-                <h1 className="text-center">Explore Recipes</h1>
-                <p className="text-center lead">or search by <span className="underline text-success">cuisine</span></p>
-            </div>
+      <Container>
+        <div className="mt-5 mb-5">
+          <h1 className="text-center">Explore Recipes</h1>
+          <p className="text-center lead">
+            or search by <span className="underline text-success">cuisine</span>
+          </p>
+        </div>
 
-            <Row className="gy-3">
-                <Col className="d-flex justify-content-center" lg={4} md={6}>
-                    <RecipeTile title="Title" author="John Doe"/>
-                </Col>
-
-                <Col className="d-flex justify-content-center" lg={4} md={6}>
-                    <RecipeTile title="Title" author="John Doe"/>
-                </Col>
-
-                <Col className="d-flex justify-content-center" lg={4}md={6}>
-                    <RecipeTile title="Title" author="John Doe"/>
-                </Col>
-            </Row>
-        </Container>
-    )
-}
+        <Row className="gy-5">
+          {recipes.map((recipe) => (
+            <Col className="d-flex justify-content-center" lg={4} md={6}>
+              <RecipeTile
+                title={recipe.recipe_title}
+                author={recipe.username}
+                image={recipe.image}
+                id={recipe.id}
+                likes={recipe.likes}
+              />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    );
+  } else {
+    return <Spinner id="spinner" animation="border" variant="success" />;
+  }
+};
 
 export default Recipes;
